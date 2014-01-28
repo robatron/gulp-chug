93045e5858ec920768fc062ca949440a2884a891
Rob McGuire-Dale <rmcguir@rei.com>
2014-01-27 20:06:29 -0800

    Clean before end of day

--- sandbox/gulpfile.js
+++ sandbox/gulpfile.js
@@ -2,6 +2,8 @@ var gulp 		= require( 'gulp' );
 var gulpCascade = require( '../index.js' );
 
 gulp.task( 'default', function () {
-    gulp.src( [ './submod/gulpfile.js', './submod/gulpfile2.js' ] )
-        .pipe( gulpCascade() )
+    gulp.src( [
+        './submod/gulpfile.js',
+        './submod/gulpfile2.js'
+    ] ).pipe( gulpCascade() )
 } );
