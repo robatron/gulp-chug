5978ee351c4a4d949daf1e52095868998f512247
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2014-01-30 09:13:45 -0800

    Add mocha test skel

---
+++ test/test-gulp-chug.js
@@ -0,0 +1,18 @@
+var assert  = require( 'assert' );
+var gulp    = require( 'gulp' );
+var chug    = require( '../index.js' );
+
+describe( 'gulp-chug', function () {
+    describe( 'in buffer mode', function () {
+        it( 'should run a gulpfile', function () {} );
+        it( 'should run a gulpfile with a custom name', function () {} );
+        it( 'should run a gulpfile with in nested directories', function () {} );
+        it( 'should run multiple gulpfiles', function () {} );
+        it( 'should run a stream modified by another plugin', function () {} );
+        it( 'should run a gulpfile in no-read mode', function () {} );
+        it( 'should ignore non-existent files', function () {} );
+    } );
+    describe( 'in streaming mode', function () {
+        it( 'should emit an error', function() {} );
+    } );
+} );
