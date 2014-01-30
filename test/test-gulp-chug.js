66e094692c01a069bbda4305eddd98d99475fa2b
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2014-01-30 09:30:00 -0800

    Add more skeleton specs

--- test/test-gulp-chug.js
+++ test/test-gulp-chug.js
@@ -4,13 +4,28 @@ var chug    = require( '../index.js' );
 
 describe( 'gulp-chug', function () {
     describe( 'in buffer mode', function () {
-        it( 'should run a gulpfile', function () {} );
-        it( 'should run a gulpfile with a custom name', function () {} );
-        it( 'should run a gulpfile with in nested directories', function () {} );
-        it( 'should run multiple gulpfiles', function () {} );
-        it( 'should run a stream modified by another plugin', function () {} );
-        it( 'should run a gulpfile in no-read mode', function () {} );
-        it( 'should ignore non-existent files', function () {} );
+        describe( 'when working with gulp sourcing', function () {
+            it( 'should run a gulpfile', function () {} );
+            it( 'should run a gulpfile with a custom name', function () {} );
+            it( 'should run a gulpfile nested in subdirectories', function () {
+                // Nested once
+
+                // Nested multiple times
+            } );
+            it( 'should run multiple gulpfiles', function () {
+                // Explicitly specified with []
+
+                // Filename wildcard globbing
+
+                // Directory wildcard globbing
+            } );
+            it( 'should ignore non-existent sources', function () {} );
+        } );
+
+        describe( 'when working with modified streams', function () {
+            it( 'should run a stream modified by another plugin', function () {} );
+            it( 'should run a gulpfile in no-read mode', function () {} );
+        } );
     } );
     describe( 'in streaming mode', function () {
         it( 'should emit an error', function() {} );
