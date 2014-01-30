ae3a9d8be6a9c0d2a53f4b89e605ed3e3ad1375f
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2014-01-30 09:41:11 -0800

    Add gulpfile template file

---
+++ test/gulpfile.tmpl.js
@@ -0,0 +1,10 @@
+/** Gulpfile template to use for testing
+*/
+var util = require( 'util' );
+var fs = require( 'fs' );
+var gulp = require( 'gulp' );
+var gutil = require( 'gulp-util' );
+
+gulp.task( 'default', function () {
+    fs.writeFileSync( 'testfile', 'testfile2' )
+} );
