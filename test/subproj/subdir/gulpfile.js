cd70557aae6d7fa8254527ed9ca7f43025b76be8
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2014-06-08 16:07:13 -0700

    Purge old 'tests'

--- test/subproj/subdir/gulpfile.js
+++
@@ -1,7 +0,0 @@
-var util = require( 'util' );
-var gulp = require( 'gulp' );
-var gutil = require( 'gulp-util' );
-
-gulp.task( 'default', function () {
-    gutil.log( util.format( 'Hello from %s!', __filename ) );
-} );
