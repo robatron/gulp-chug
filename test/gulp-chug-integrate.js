8d2598ce78b893bd7e88e4262649ef6cb3376307
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2014-06-08 22:53:44 -0700

    Restore integration 'tests'

---
+++ test/gulp-chug-integrate.js
@@ -0,0 +1,68 @@
+/**
+ * gulp-chug integration "tests" to assure a working state in a less-contrived
+ * environment.
+ */
+var gulp        = require( 'gulp' );
+var replace     = require( 'gulp-replace' );
+var chug        = require( '../index.js' );
+
+// Happy path
+gulp.task( 'happy', function () {
+    gulp.src( './subproj/gulpfile.js' )
+        .pipe( chug() )
+} );
+
+// Custom gulpfile file name
+gulp.task( 'custom-filename', function () {
+    gulp.src( './subproj/gulpfile-custom-name.js' )
+        .pipe( chug() )
+} );
+
+// Nested gulpfile
+gulp.task( 'deep-nest', function () {
+    gulp.src( './subproj/subdir/gulpfile.js' )
+        .pipe( chug() )
+} );
+
+// Glob multiple gulpfiles
+gulp.task( 'glob', function () {
+    gulp.src( './subproj/**/gulpfile*.js' )
+        .pipe( chug() )
+} );
+
+// Non-existant gulpfile
+gulp.task( 'non-existant', function () {
+    gulp.src( './subproj/non-existant-file.js' )
+        .pipe( chug() )
+} );
+
+// No-read option
+gulp.task( 'no-read', function () {
+    gulp.src( './subproj/gulpfile.js', { read: false } )
+        .pipe( chug() )
+} );
+
+// Mess with the gulpfile before running
+gulp.task( 'modify-before', function () {
+    gulp.src( './subproj/gulpfile.js' )
+        .pipe( replace( 'Hello', 'Goodbye' ) )
+        .pipe( chug() )
+} );
+
+// Make sure it works synchronously
+gulp.task( 'sync', function () {
+    return gulp.src( './subproj/gulpfile.js' )
+        .pipe( chug() )
+} );
+
+
+gulp.task( 'default', [
+    'happy',
+    'custom-filename',
+    'deep-nest',
+    'glob',
+    'non-existant',
+    'no-read',
+    'modify-before',
+    'sync'
+] );
---
+++ test/subproj/subdir/gulpfile.js
@@ -0,0 +1,7 @@
+var util = require( 'util' );
+var gulp = require( 'gulp' );
+var gutil = require( 'gulp-util' );
+
+gulp.task( 'default', function () {
+    gutil.log( util.format( 'Hello from %s!', __filename ) );
+} );
