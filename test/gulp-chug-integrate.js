62754bd4a61c22fdd46a51fbf30057875919ba98
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2014-06-08 23:27:58 -0700

    Make all integration tests sync

--- test/gulp-chug-integrate.js
+++ test/gulp-chug-integrate.js
@@ -13,53 +13,46 @@ var chug        = require( '../index.js' );
 
 // Happy path
 gulp.task( 'happy', function () {
-    gulp.src( './subproj/gulpfile.js' )
+    return gulp.src( './subproj/gulpfile.js' )
         .pipe( chug() )
 } );
 
 // Custom gulpfile file name
 gulp.task( 'custom-filename', function () {
-    gulp.src( './subproj/gulpfile-custom-name.js' )
+    return gulp.src( './subproj/gulpfile-custom-name.js' )
         .pipe( chug() )
 } );
 
 // Nested gulpfile
 gulp.task( 'deep-nest', function () {
-    gulp.src( './subproj/subdir/gulpfile.js' )
+    return gulp.src( './subproj/subdir/gulpfile.js' )
         .pipe( chug() )
 } );
 
 // Glob multiple gulpfiles
 gulp.task( 'glob', function () {
-    gulp.src( './subproj/**/gulpfile*.js' )
+    return gulp.src( './subproj/**/gulpfile*.js' )
         .pipe( chug() )
 } );
 
 // Non-existant gulpfile
 gulp.task( 'non-existant', function () {
-    gulp.src( './subproj/non-existant-file.js' )
+    return gulp.src( './subproj/non-existant-file.js' )
         .pipe( chug() )
 } );
 
 // No-read option
 gulp.task( 'no-read', function () {
-    gulp.src( './subproj/gulpfile.js', { read: false } )
+    return gulp.src( './subproj/gulpfile.js', { read: false } )
         .pipe( chug() )
 } );
 
 // Mess with the gulpfile before running
 gulp.task( 'modify-before', function () {
-    gulp.src( './subproj/gulpfile.js' )
+    return gulp.src( './subproj/gulpfile.js' )
         .pipe( replace( 'Hello', 'Goodbye' ) )
         .pipe( chug() )
 } );
-
-// Make sure it works synchronously
-gulp.task( 'sync', function () {
-    return gulp.src( './subproj/gulpfile.js' )
-        .pipe( chug() )
-} );
-
 
 gulp.task( 'default', [
     'happy',
@@ -68,6 +61,5 @@ gulp.task( 'default', [
     'glob',
     'non-existant',
     'no-read',
-    'modify-before',
-    'sync'
+    'modify-before'
 ] );
