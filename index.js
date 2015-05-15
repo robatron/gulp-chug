f09f6d6deea8b341a86514801940774c1beb3ef0
Chklang <delphin.germa@gmail.com>
2015-05-15 09:07:24 +0200

    Use gutil.log to log

--- index.js
+++ index.js
@@ -34,8 +34,7 @@ module.exports = function ( options ) {
 
         // Configure logging and errors
         var say = function( msg, noNewLine ) {
-            var sayFn = noNewLine ? util.print : console.log;
-            sayFn( util.format( '[%s]', gutil.colors.green( PKG.name ) ), msg );
+            gutil.log( util.format( '[%s]', gutil.colors.green( PKG.name ) ), msg );
         };
 
         var sayErr = function( errMsg ) {
